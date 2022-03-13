85d76aaa222b8d4a4affec6349abe3bfebf2048b
idiallo7 <idiallo@gwu.edu>
2022-03-13 13:48:33 -0400

    fixed home jdaanalyticsapp link

--- jdaanalyticsapp/urls.py
+++ jdaanalyticsapp/urls.py
@@ -7,7 +7,4 @@ urlpatterns = [
     path('jdaanalyticsapp_upload_form/', views.jdaanalyticsapp_upload_form, name='jdaanalyticsapp_upload_form'),
     path('jdaanalyticsapp_rpt/', views.jdaanalyticsapp_rpt, name='jdaanalyticsapp_rpt'),
     path('jdaanalyticsapp_sec_filter/', views.jdaanalyticsapp_sec_filter, name='jdaanalyticsapp_sec_filter'),
-    #path('jdaanalyticsapp_view_book>', views.jdaanalyticsapp_view_book, name='jdaanalyticsapp_view_book'),
-    #path('jdaanalyticsapp_create_book/<int:pk>', views.jdaanalyticsapp_create_book, name='jdaanalyticsapp_create_book')
-
 ]
