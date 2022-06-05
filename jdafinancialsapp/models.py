9da9547a20e86185e73d328413a8a43ede9fae63
idiallo7 <idiallo@gwu.edu>
2022-06-05 18:42:37 -0400

    Fix migration issues 5

--- jdafinancialsapp/models.py
+++ jdafinancialsapp/models.py
@@ -186,6 +186,19 @@ class EconomicZoneModel(models.Model):
         db_table = 'EconomicZoneModel'
         unique_together = [['country', 'econ_zone']]
 
+
+
+#////////////////////////// TestModel ///////////////////
+class ResModel(models.Model):
+    name= models.CharField(max_length=20)
+    country = CountryField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
 # /////////////////////////////////// FinancialStatementModel ///////////////////////////////
 class FinancialStatementModel(models.Model):
     name = models.CharField(max_length=50, unique=True)
