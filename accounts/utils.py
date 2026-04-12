c702dbac7cfdaba1ce98956dfeb558c9f9e194b5
idiallo7 <idiallo@gwu.edu>
2026-04-12 18:39:35 -0400

    Fix syntax error: replace curly quotes with straight quotes in utils.py

--- accounts/utils.py
+++ accounts/utils.py
@@ -24,12 +24,12 @@ def image_resize(logo, width, height):
     # check if either the width or height is greater than the max
     if img.width > width or img.height > height:
         output_size = (width, height)
-        # Create a new resized “thumbnail” version of the image with Pillow
+        # Create a new resized thumbnail version of the image with Pillow
         img.thumbnail(output_size)
         # Find the file name of the image
         img_filename = Path(logo.name).name
-        # Spilt the filename on “.” to get the file extension only
-        img_suffix = Path(logo.name).name.split(“.”)[-1]
+        # Split the filename on "." to get the file extension only
+        img_suffix = Path(logo.name).name.split(".")[-1]
         # Use the file extension to determine the file type from the image_types dictionary
         img_format = image_types[img_suffix]
         # Save the resized image into the buffer, noting the correct file type
